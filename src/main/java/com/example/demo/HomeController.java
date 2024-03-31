93c85312a659801ce93bb8e2d8b6186c7a20b7f1
aleix <aleixm@gmail.com>
2024-03-31 13:13:53 +0200

    Added authorities to jwt

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,13 +2,22 @@ package com.example.demo;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import okhttp3.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
@@ -20,21 +29,45 @@ public class HomeController {
     }
 
     @RequestMapping("/")
-    public String home(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model, HttpServletResponse response, Saml2Authentication authentication) {
+    public String home(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model, Saml2Authentication authentication) {
         model.addAttribute("name", principal.getName());
         model.addAttribute("emailAddress", principal.getFirstAttribute("email"));
         model.addAttribute("userAttributes", principal.getAttributes());
-        System.out.println(principal.getAttributes());
 
+        System.out.println(principal.getAttributes());
         System.out.println(authentication.getSaml2Response());
 
-        String role = "USER";
-        String jwt = jwtService.generateToken(principal.getName());
-        Cookie cookie = new Cookie("token", jwt);
-        cookie.setMaxAge(60*60*24);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-        return "redirect:http://localhost:8081/api/v1/users/list";
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String jwt = jwtService.generateToken(principal.getName(), authorities);
+
+        System.out.println(authorities);
+
+        String username = principal.getName();
+
+        HttpHeaders headers = new org.springframework.http.HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + jwt);
+        model.addAttribute("token", jwt);
+        model.addAttribute("username", username);
+        model.addAttribute("authorities", authorities.stream().toList());
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
+
+        return "success";
     }
 
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/demo/JWTService.java
+++ src/main/java/com/example/demo/JWTService.java
@@ -3,24 +3,32 @@ package com.example.demo;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class JWTService {
 
     private static final String SECRET_KEY = "tBTeEle6IfDgxVXwH0s7bp0aPhQpW9Bw/tppsLTyMJ580KlH1g6ZULwpk5270frEhCtBoditMX9TlBhhZSrlSg==";
 
-    private static final long EXPIRE_DATE = 1000*60*24;
+    private static final long EXPIRE_DATE = 1000*60*60;
 
-    public String generateToken(String username) {
+    public String generateToken(String username, Collection<? extends GrantedAuthority> authorities) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + EXPIRE_DATE);
 
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()));
+
         return Jwts.builder()
                 .subject(username)
+                .claims(claims)
                 .issuedAt(now)
                 .expiration(expiryDate)
                 .signWith(generateKey())
